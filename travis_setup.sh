0861a5e0f331013d6f4205a947c0cacb065cb661
Irfan Baqui <irfan.baqui@gmail.com>
2015-08-21 11:21:32 -0700

    made dir

---
+++ travis_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir testdir;
+git add .;
+git commit -m "made dir";
+git push origin master;
+
+# if [[ $TRAVIS_BRANCH == '1.0.-stable' ]]
+#   cd test/dummy
+#   rake db:schema:load
+# else
+#   cd spec/dummy
+#   rake db:schema:load
+# fi
