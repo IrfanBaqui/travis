4ef8be5ef81c852303585c00505f8ebf995adc62
Irfan Baqui <irfan.baqui@gmail.com>
2015-08-21 13:40:36 -0700

    stuff

--- travis_setup.sh
+++ travis_setup.sh
@@ -5,9 +5,9 @@
 # git commit -m "made dir";
 # git push origin master;
 
-echo "Hello there!";
+# echo "Hello there!";
 
-
+git clone -b test_branch --single-branch git@github.com:IrfanBaqui/travis.git
 
 # if [[ $TRAVIS_BRANCH == '1.0.-stable' ]]
 #   cd test/dummy
